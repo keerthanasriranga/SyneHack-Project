3e7911f25f5d1b193d868ef5843b260d4f6887c3
Tazeen <tazeenmunnavar33@gmail.com>
2017-04-17 02:07:53 +0530

    Initial commit

---
+++ demo_simple.py
@@ -0,0 +1,21 @@
+import cmu_sphinx4
+import os
+
+def from_cwd(relative_path):
+    return os.path.join(os.getcwd(), relative_path.lstrip('\/'))
+
+# this must be a canonical URL, with no relative path symbols such as ..
+# WARNING: the audio file specified MUST be 16 kHz 16 bit mono files in
+# MS WAV format.
+audio_URL = 'file://localhost' + from_cwd('audio/call1.wav')
+
+transcriber = cmu_sphinx4.Transcriber(audio_URL)
+
+# due to some initialization in Sphinx-4, it may take 30 - 60 seconds before you
+# start seeing any transcriptions
+f = open('1.txt', 'w')
+for line in transcriber.transcript_stream():
+	print >> f, line
+	
+f.close()
+transcriber.close()
